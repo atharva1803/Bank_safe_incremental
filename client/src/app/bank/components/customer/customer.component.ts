de4a8f3f26a188e2ac4821df7b4acf465b431c58
atharva1803 <atharvagotmare28@gmail.com>
2026-05-01 19:35:08 +0000

    Day-21

--- client/src/app/bank/components/customer/customer.component.ts
+++ client/src/app/bank/components/customer/customer.component.ts
@@ -1,45 +1,58 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { CustomerTS } from '../../types/tstypes/Customerts';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-customers',
   templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.scss']
+  styleUrls: ['./customer.component.scss'],
 })
-export class CustomersComponent {
-
-  isFormSubmitted: boolean | undefined;
-  customerSuccess$: any;
-  customerError$: any;
+export class CustomersComponent implements OnInit {
+  isFormSubmitted = false;
+  customerSuccess: string = '';
+  customerError: string = '';
   customerForm!: FormGroup;
-  customer: CustomerTS;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.customer = new CustomerTS("", "", "", "", "", "");
-  }
+  customers: CustomerTS[] = [
+    new CustomerTS('John Doe', 'john@example.com', 'john_doe', 'password123', 'User', '1'),
+    new CustomerTS('John Doe1', 'john1@example.com', 'john_doe1', 'password123', 'Admin', '2'),
+  ];
 
+  constructor(private formBuilder: FormBuilder) { }
+  
   ngOnInit(): void {
     this.customerForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      email: ['', Validators.required],
-      username: ['', Validators.required],
-      password: ['', Validators.required]
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', [Validators.required, this.noSpecialCharacters]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+        ],
+      ],
     });
+  }
+
+  private noSpecialCharacters(control: any): { [key: string]: boolean } | null {
+    const SPECIAL_CHARACTERS_REGEX = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/;
+    if (SPECIAL_CHARACTERS_REGEX.test(control.value)) {
+      return { specialCharacters: true };
+    }
+    return null;
   }
 
   onSubmit(): void {
     this.isFormSubmitted = true;
 
-    const value = this.customerForm.value;
-
-    this.customer = new CustomerTS(
-      value.name,
-      value.email,
-      value.username,
-      value.password,
-      "",
-      ""
-    );
+    if (this.customerForm.valid) {
+      const { name, email, username, password } = this.customerForm.value;
+      this.customerSuccess = `Customer created successfully! Name: ${name}, Email: ${email}, Username: ${username}.`;
+      this.customerError = ''; 
+    } else {
+      this.customerError = 'Please correct the errors in the form.';
+      this.customerSuccess = ''; 
+    }
   }
 }
